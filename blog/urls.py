73fb687e696446805fa7822bf01713f0050d5d66
Tushar Srivastava <tushar@gmail.com>
2017-07-10 00:39:13 +0530

    staticfiles

--- blog/urls.py
+++ blog/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('accounts.urls')),
     url(r'^', include('posts.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
